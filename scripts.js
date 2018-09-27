60fd0d87aa7dda00d500c57ac97e92a99eef05e1
Landon Henrie <Looney1300@gmail.com>
2018-09-27 09:26:47 -0600

    working awesomely. More product info, and search bar working.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 (function () {
-  console.log('did it');
   $("#main").load("loads/home.html");
-})();
+})(); // Immediately executed function on script load.
+
+$("#nav_home").click(function() {
+  $("#main").load("loads/home.html");
+});
 
 $("#load_nutrition").click(function() {
   $("#main").load("loads/nutrition.html");
@@ -17,5 +20,13 @@ $("#load_bath").click(function() {
 
 $("#load_home").click(function() {
   $("#main").load("loads/cleaning.html");
+});
+
+$("#searchbutton").click(function() {
+  //http://www.amway.com/Shop/Search/SearchResults.aspx?searchkeyword=laundry%20wash&includeLMS=false&pwsID=Henrie
+  let searchterm = document.getElementById('searchbar').value.replace(' ', '%20');
+  let searchurl = 'http://www.amway.com/Shop/Search/SearchResults.aspx?searchkeyword='+ searchterm +'&includeLMS=false&pwsID=Henrie';
+  let newtab = window.open(searchurl, '_blank');
+  newtab.focus();
 });
 
