b0741c08a55799a9f88f18bb855aa733c0b04643
Landon Henrie <Looney1300@gmail.com>
2018-10-27 12:08:00 -0600

    Finished some cleanup on styles and added lipgloss and reviews finished.

--- loads/beauty_database.js
+++ loads/beauty_database.js
@@ -11,6 +11,13 @@
 
 jsdb.beauty = [
     {
+        imgsrc: 'assets/lipgloss.jpg',
+        title: 'Artistry Signature Color® Light Up Lip Gloss',
+        price: '$19.00',
+        description: 'Light up your lips to mirror your mood. Extra moisturizing formula – enriched with jojoba and avocado oils, give lips a luxurious, vibrant shine.',
+        href: 'http://www.amway.com/Shop/Product/Product.aspx?itemNo=4036&pwsID=Henrie'
+    },
+    {
         imgsrc: 'assets/foamcleanser.jpg',
         title: 'Artistry™ Advanced Creamy Foam Cleanser',
         price: '$27.00',
